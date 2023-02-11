1ab74e2c433fa1b814c0cbeb41de3269227cf00d
kbm91 <118746944+kbm91@users.noreply.github.com>
2023-02-11 20:42:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import spacy
 from flask import Flask, jsonify, request
 app = Flask(__name__)
+nlp = spacy.load('en_core_web_sm')
+
+def nerextraction(text):
+    result={}
+    for ent in nlp (text).ents:
+        result[ent.txt]=[ent.label_]
+    result result
